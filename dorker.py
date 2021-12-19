a5c3e093c26fc90964572078791cc40f305372a0
Luis Javier <60628803+luijait@users.noreply.github.com>
2021-12-19 03:39:18 +0100

    Docker V1.0

---
+++ dorker.py
@@ -0,0 +1,140 @@
+import requests
+import sys
+import urllib
+from requests_html import HTML
+from requests_html import HTMLSession
+def fsociety():
+    fs = '''
+XX                                                                          XX
+XX   MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMMssssssssssssssssssssssssssMMMMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+XX   MMMMMy''                                                    ''yMMMMM   XX
+XX   MMMy'                                                          'yMMM   XX
+XX   Mh'                                                              'hM   XX
+XX   -                                                                  -   XX
+XX                                                                          XX
+XX   ::                                                                ::   XX
+XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+XX                                oo      oo                                XX
+XX           oM                 oo          oo                 Mo           XX
+XX         oMMo                M              M                oMMo         XX
+XX       +MMMM                 s              s                 MMMM+       XX
+XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
+XX                                                                          XX
+
+    .o88o.                               o8o                .
+    888 `"                               `"'              .o8
+   o888oo   .oooo.o  .ooooo.   .ooooo.  oooo   .ooooo.  .o888oo oooo    ooo
+    888    d88(  "8 d88' `88b d88' `"Y8 `888  d88' `88b   888    `88.  .8'
+    888    `"Y88b.  888   888 888        888  888ooo888   888     `88..8'
+    888    o.  )88b 888   888 888   .o8  888  888    .o   888 .    `888'
+   o888o   8""888P' `Y8bod8P' `Y8bod8P' o888o `Y8bod8P'   "888"      d8'
+                                                                .o...P'
+                                                                `XER0'
+
+'''
+
+    return fs
+def menu(fs):
+    print (fs)
+    print("1. Camaras de seguridad")
+    print("2. Paginas vulnerables a SQL")
+    print("3. Paginas vulnerables a LFI")
+    print("4. Dork personalizada (Introducir tu propia dork)")
+    print("5. Salir del Programa")
+def codigo_fuente_pagina(url):
+
+    try:
+       url = url
+       session = HTMLSession()
+       print (url)
+       source = session.get(url)
+      # res = requests.Response.code(source)
+       return source
+    except requests.exceptions.RequestException:
+        print("Algo ha salido mal revise la URL")
+def scrapper(consulta):
+    consulta = urllib.parse.quote_plus(consulta)
+    resultados = codigo_fuente_pagina("https://www.google.co.uk/search?q=" + consulta)
+
+    enlaces = list(resultados.html.absolute_links)
+    dominios_google = ('https://www.google.',
+                      'https://google.',
+                      'https://webcache.googleusercontent.',
+                      'http://webcache.googleusercontent.',
+                      'https://policies.google.',
+                      'https://support.google.',
+                      'https://maps.google.',
+                      'https://translate.google.')
+    salida = []
+    for link in enlaces[:]:
+        if link.startswith(dominios_google):
+            enlaces.remove(link)
+    #for enlace in enlaces[:]:
+        #enlace2 = requests.get(enlace)
+        #item = {
+         #   'link': enlace
+            #'Response Code': enlace2.status_code
+        #}
+        #salida.append(item)
+        for enlace in enlaces:
+            print ('Enlace:' + enlace + '\n')
+if __name__ == '__main__':
+    while True:
+        fs = fsociety()
+        menu(fs)
+        choice = input('Introduce que quieres buscar: ')
+        if choice == '1':
+            consulta = 'inurl:guestimage.html'
+        if choice == '2':
+            consulta = 'inurl:php=id? OR storemanager/contents/item.php?page_code='
+
+        if choice == '3':
+            consulta = 'inurl:/includes/header.php?systempath='
+        if choice == '4':
+            consulta = input("Introduce cual es la dork que quieres sacar")
+        if choice == 5 :
+            sys.exit()
+        print(scrapper(consulta))
+        nextloop = input('Quieres continuar usando el programa? (Y/N): ')
+        if nextloop == 'Y' or nextloop == 'y':
+            pass
+        else:
+            print('[+] Finalizando programa')
+            print('[+] Recuerda F Society no es lo mismo que Fsociety')
+            break
+
+
